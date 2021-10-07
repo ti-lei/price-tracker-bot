3da7e49c58815ad2530d5bc3d17a9ccab90bba07
layx <layx@cathaylife.com.tw>
2021-10-07 17:29:49 +0800

    'deploy-20211007'

--- pt_config.py
+++ pt_config.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 env_path = os.path.join(BASE_DIR, '.env')
 load_dotenv(dotenv_path=env_path, verbose=True)
-
+print(env_path)
 TELEGRAM_BOT_MODE = os.getenv('TELEGRAM_BOT_MODE', 'polling')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 MOMO_URL = 'https://m.momoshop.com.tw/'
@@ -21,11 +21,12 @@ USER_SUB_GOOD_LIMITED = 11
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
+print(DB_HOST,DB_NAME,DB_USER)
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 LOGGING_LEVEL = os.getenv('LOGGING_LEVEL')
 PERIOD_HOUR = int(os.getenv('PERIOD_HOUR', 2))
-
+print(DB_PASSWORD)
 MOMO_REQUEST_TIMEOUT = 10
 
 def momo_good_url():
